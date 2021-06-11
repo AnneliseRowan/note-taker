ba328a63478b89f8728f40d06ca52c9793a52a7b
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-06-11 14:10:17 -0400

    got it push to db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,40 @@
 const db = require("../db/db.json"); 
 const fs = require("fs");
 
+
 module.exports = (app) => {
 
-    app.get("/api/notes", (req, res) => res.json(db));
-    app.get("/api/notes/:id", (req, res) => res.json(db[req.params.id])); 
+    fs.readFile("./db/db.json", "utf8", (data) => {
+        let notes = JSON.parse(data); 
 
-
-    app.post("/api/notes", (req, res) => {
-        updateDb(req.body); 
-        return(`Added new note: ${req.body.title}`);
-    });
-
-    app.delete("/api/notes/:id", (req, res) => {
-        db.splice(req.params.id, 1); 
-        updateDb(); 
-        console.log(`Deleted noted with id: ${req.params.id}`);
-    });
-
-    const updateDb = (newNote) => {
-        // fs.readFile("db/db.json", "utf8", (data) => {
+        app.get("/api/notes", (req, res) => {
+            res.json(notes)
+        });
+    
+    
+        app.post("/api/notes", (req, res) => {
+            let newNote = req.body; 
+            updateDb(newNote); 
+            return console.log(`Added new notes: ${newNote}`)        
+    
+        });
+    
+        app.delete("/api/notes/:id", (req, res) => {
+    
+            notes.splice(req.params.id, 1); 
+            updateDb(); 
+            console.log(`Deleted noted with id: ${req.params.id}`); 
             
-        // })
-        fs.writeFile("../db/db.json", JSON.stringify(newNote, "\t"), err => {
-            if(err) {console.log("Ahhhhhh :", err)}; 
-            console.log("Voila!"); 
-        })
-    }
+        });
+    
+        const updateDb = (newNote) => {
+    
+            fs.writeFile("./db/db.json", JSON.stringify(newNote), (err) => {
+                if (err) {console.log("Ahhhhhh :", err)}; 
+                console.log("Voila!"); 
+                return true; 
+            })
+    
+        }
+    })
 }
