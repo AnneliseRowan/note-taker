95aa50ffba6c5db51c15d64f35699c117e7a4698
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-06-13 13:58:26 -0400

    trying to get heroku to work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express"); 
 
 const app = express(); 
-const port = process.env.port || 3030; 
+const PORT = process.env.PORT || 3030; 
 
 app.use(express.urlencoded({ extended:true })); 
 app.use(express.json()); 
@@ -12,6 +12,6 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app); 
 
 
-app.listen(process.env.port || 3030, () => {
+app.listen(PORT, () => {
     console.log(`App listening on port: ${port}`); 
 }); 
